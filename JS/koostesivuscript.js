9e97998393d041b3866b34f80f19c66c11707215
rosa-liina <rosa.liina.keto@gmail.com>
2024-12-11 12:33:27 +0200

    minor changes to the page "koostesivu"

--- JS/koostesivuscript.js
+++ JS/koostesivuscript.js
@@ -1,31 +1,27 @@
 //Elementit:
-//Muutetaan tulos numeroksi ja tulos on 0 jos peliä ei ole pelattu.
+//Muutetaan tulos numeroksi ja tulos on 0, jos peliä ei ole pelattu.
 const ravintotietousScore = parseInt(sessionStorage.getItem("ravintotietousScore")) || 0;
 const kasvikisailuScore = parseInt(sessionStorage.getItem("kasvikisailuScore")) || 0;
 const luontopoliisiScore = parseInt(sessionStorage.getItem("luontopoliisiScore")) || 0;
 const villivisailuScore = parseInt(sessionStorage.getItem("villivisailuScore")) || 0;
 
 const ravintotietousresultsElement = document.getElementById("ravintotietous-results");
+const villivisailuresultsElement = document.getElementById("ravintotietous-results");
+const luontopoliisiresultsElement = document.getElementById("ravintotietous-results");
+const kasvikisailuresultsElement = document.getElementById("ravintotietous-results");
 
 const totalScore = ravintotietousScore + kasvikisailuScore + luontopoliisiScore + villivisailuScore;
 const totalScoreElement = document.getElementById("total-score");
 totalScoreElement.textContent = `Yhteensä: ${totalScore} pistettä`;
 
-// Ravintotietous-pelin pisteet:
+// Pelien pisteet:
 if (ravintotietousScore !== null) {
     ravintotietousresultsElement.textContent = `Tulos: ${ravintotietousScore}/10`;
 } else {
     ravintotietousresultsElement.textContent = `Ei tulosta.`;
 }
 
-//Yhteispisteet:
-if (totalScore > 0) {
-    totalScoreElement.textContent = `Yhteensä: ${totalScore}/40 pistettä`;
-} else {
-    totalScoreElement.textContent = `Ei yhteispisteitä.`;
-}
 
-/*
 if (villivisailuScore !== null) {
     villivisailuresultsElement.textContent = `Tulos: ${villivisailuScore}/10`;
 } else {
@@ -44,4 +40,11 @@ if (kasvikisailuScore !== null) {
     kasvikisailuresultsElement.textContent = `Tulos: ${kasvikisailuScore}/10`;
 } else {
     kasvikisailuresultsElement.textContent = `Ei tulosta.`;
-} */
+}
+
+//Yhteispisteet:
+if (totalScore > 0) {
+    totalScoreElement.textContent = `${totalScore}/40 pistettä`;
+} else {
+    totalScoreElement.textContent = `0/40`;
+}
