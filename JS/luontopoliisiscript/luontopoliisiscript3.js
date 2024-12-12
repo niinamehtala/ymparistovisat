f9ed88353ba0664db43a4ba60b3928f76870988c
n3koai00 <kontio.iiris02@gmail.com>
2024-12-12 21:35:44 +0200

    Added full version of game, lacks sessionstorage

--- JS/luontopoliisiscript/luontopoliisiscript3.js
+++ JS/luontopoliisiscript/luontopoliisiscript3.js
@@ -1,5 +1,9 @@
 window.onload = function() {
     const urlParams = new URLSearchParams(window.location.search);
     const score = urlParams.get('score');
-    document.getElementById("score").textContent = `Sait ${score} pistettä!`;
+    document.getElementById("score").textContent = `Sait ${score}/4 oikein!`;
 };
+
+function addPoints() {
+    sessionStorage.setItem('luontopoliisiScore', points.toString())
+}
